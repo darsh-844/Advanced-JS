fd16b9bafe5e8b8ab4a4de2d7a91f26c41847ee2
darsh-844 <darshjaiswal2007@gmail.com>
2026-08-18 13:10:39 +0530

    ADJS-01

---
+++ commonjs.js
@@ -0,0 +1,7 @@
+// const {calc_area , calc_peri }= require('./main')
+// let r=3;
+// console.log(calc_area(r));
+// console.log(calc_peri(r));
+
+import isVote from './esm.js'
+console.log(isVote(19));
