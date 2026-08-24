50e61c654dd81fa14f932e9ee3294b2a648fd385
darsh-844 <darshjaiswal2007@gmail.com>
2026-08-24 10:03:04 +0530

    Promises

--- promises.js
+++ promises.js
@@ -1,30 +1,9 @@
-const promise1 = new Promise((resolve, reject) => {
-    let success = true;
-    if (success) {
-        resolve({
-            id: 29032030,
-            name: "John Doe",
-        });
-    }
-    else {
-        reject({
-            error: "Something went wrong",
-        });
-    }
-});
-
-// promise1.then((response) => {
-//     console.log(response);
-// }).catch((error) => {
-//     console.log(error);
-// });
-
-// const promise2 = new Promise((resolve, reject) => {
+// const promise1 = new Promise((resolve, reject) => {
 //     let success = true;
-//     if (success) {  
+//     if (success) {
 //         resolve({
-//             id: 29032031,
-//             name: "Jane Doe",
+//             id: 29032030,
+//             name: "John Doe",
 //         });
 //     }
 //     else {
@@ -34,19 +13,59 @@ const promise1 = new Promise((resolve, reject) => {
 //     }
 // });
 
+// // promise1.then((response) => {
+// //     console.log(response);
+// // }).catch((error) => {
+// //     console.log(error);
+// // });
 
-// Promise.any([promise1, promise2]).then((responses) => {
-//     console.log(responses);
-// }).catch((error) => {
+// // const promise2 = new Promise((resolve, reject) => {
+// //     let success = true;
+// //     if (success) {  
+// //         resolve({
+// //             id: 29032031,
+// //             name: "Jane Doe",
+// //         });
+// //     }
+// //     else {
+// //         reject({
+// //             error: "Something went wrong",
+// //         });
+// //     }
+// // });
+
+
+// // Promise.any([promise1, promise2]).then((responses) => {
+// //     console.log(responses);
+// // }).catch((error) => {
+// //     console.log(error);
+// // });
+
+// promise1.then((response) => {
+//     return response
+// })
+// .then((response2) => {
+//     console.log(response2.username);
+// })
+// .catch((error) => {
 //     console.log(error);
-// });
+// })
 
-promise1.then((response) => {
-    return response
-})
-.then((response2) => {
-    console.log(response2.username);
-})
-.catch((error) => {
-    console.log(error);
-})
+
+function fetchUserData() {
+    return new Promise((resolve, reject) => {
+        let success = true;
+        if (success) {
+            resolve({
+                id: 29380083,
+                name: "John Doe",
+                email: "john.doe@example.com"
+            });
+        }
+        else {
+            reject({
+                error: "Failed to fetch user data"
+            });
+        }
+    });
+}
