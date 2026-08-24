ba3745dba1081c762a49d16d97fc79876693970b
darsh-844 <darshjaiswal2007@gmail.com>
2026-08-24 09:37:23 +0530

    Promises

--- promises.js
+++ promises.js
@@ -1,6 +1,42 @@
-const user = { id: 1, username: "Zlatan" };
-const profile = { id: 1, userId: 1, bio: "I am Zlatan" };
-const posts = ["Post 1", "Post 2", "Post 3"];
+const promise1 = new Promise((resolve, reject) => {
+    let success = true;
+    if (success) {
+        resolve({
+            id: 29032030,
+            name: "John Doe",
+        });
+    }
+    else {
+        reject({
+            error: "Something went wrong",
+        });
+    }
+});
+
+// promise1.then((response) => {
+//     console.log(response);
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+const promise2 = new Promise((resolve, reject) => {
+    let success = true;
+    if (success) {  
+        resolve({
+            id: 29032031,
+            name: "Jane Doe",
+        });
+    }
+    else {
+        reject({
+            error: "Something went wrong",
+        });
+    }
+});
 
 
-
+Promise.all([promise1, promise2]).then((responses) => {
+    console.log(responses);
+}).catch((error) => {
+    console.log(error);
+});
