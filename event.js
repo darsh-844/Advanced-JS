2ab8772a8510e8b7c274f3a224372c80d7979973
darsh-844 <darshjaiswal2007@gmail.com>
2026-08-25 12:47:58 +0530

    EVENTjs

--- event.js
+++ event.js
@@ -1,21 +1,31 @@
 import fs from "fs";
 
-setTimeout(() => {
-  console.log("This message is displayed after 2 seconds");
-}, 2000);
+// setTimeout(() => {
+//   console.log("This message is displayed after 2 seconds");
+// }, 2000);
+
+// fs.readFile("intro.txt", "utf8", (err, data) => {
+//   if (err) {
+//     console.error("Error reading file:", err);
+//     return;
+//   }
+// });
+
+// setInterval(() => {
+//   console.log("This message is displayed every 3 seconds");
+// }, 3000);   
+
+// setImmediate(() => {
+//   console.log("This message is displayed immediately after the current event loop phase");
+// });
 
 fs.readFile("intro.txt", "utf8", (err, data) => {
-  if (err) {
-    console.error("Error reading file:", err);
-    return;
-  }
+    console.log("File read completed");
+    setTimeout(() => {
+        console.log("This message is displayed after 2 seconds");
+    }, 0);
+
+    setImmediate(() => {
+        console.log("This message is displayed immediately after the current event loop phase");
+    });
 });
-
-setInterval(() => {
-  console.log("This message is displayed every 3 seconds");
-}, 3000);   
-
-setImmediate(() => {
-  console.log("This message is displayed immediately after the current event loop phase");
-});
-
