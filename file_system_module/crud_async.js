474e6fc8c86db6bc46539d8d59d1e3b73d6a415e
darsh-844 <darshjaiswal2007@gmail.com>
2026-08-31 21:21:24 +0530

    CRUD

---
+++ file_system_module/crud_async.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+fs.writeFile("notes.txt", "Hello, World!", (err) => {
+    if (err) {
+        console.error (err);
+        return;
+    }
+    console.log("File created successfully.");
+});
+
+fs.readFile("notes.txt", "utf8", (err, data) => {
+ if (err) {
+        console.error(err);
+        return;
+    }
+    console.log("Read data from notes.txt:", `${data}`);
+});     
+
+const updateData = "hello , ece 3rd sem";
+fs.appendFile("notes.txt", `\n${updateData}`, (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log("file updated successfully.");
+});
+
+fs.rm("notes.txt", (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }   
+    console.log("File deleted successfully.");
+});
