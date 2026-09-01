3707a274e2c3e3210c94bbf72b58a555b1b2fd50
darsh-844 <darshjaiswal2007@gmail.com>
2026-09-01 12:38:28 +0530

    Stats

--- file_system_module/stats.js
+++ file_system_module/stats.js
@@ -1,16 +1,32 @@
-import fs from 'fs'
+const fs = require('fs');
 
-fs.stat("notes.txt",(err, stats)=>{
+// fs.stat("notes.txt",(err, stats)=>{
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log("Information of [notes.txt]", stats);
+//     console.log("Size of the file:", stats.size,"Bytes");
+//     console.log("Creation time of file:", stats.birthtime.toISOString().split('T'));
+//     console.log("Modified time of file:", stats.mtime.toISOString().split('T'));
+//     console.log("Access time of file:",stats.atime.toISOString().split('T'));
+//     console.log("Change time of file:", stats.ctime.toISOString().split('T'));
+//     console.log("Is file:", stats.isFile());
+//     console.log("Is directory:", stats.isDirectory());
+// });
+
+fs.stat("./myfolder1",(err, stats)=>{
     if(err){
-        console.error(err);
+        console.log(err);
         return;
     }
-    console.log("Information of [notes.txt]", stats);
-    console.log("Size of the file:", stats.size,"Bytes");
-    console.log("Creation time of file:", stats.birthtime.toISOString().split('T'));
-    console.log("Modified time of file:", stats.mtime.toISOString().split('T'));
-    console.log("Access time of file:",stats.atime.toISOString().split('T'));
-    console.log("Change time of file:", stats.ctime.toISOString().split('T'));
+    console.log("Information of [myfolder1]", stats);
+    console.log("Size of the folder:", stats.size,"Bytes");
+    console.log("Creation time of folder:", stats.birthtime.toISOString().split('T'));
+    console.log("Modified time of folder:", stats.mtime.toISOString().split('T'));
+    console.log("Access time of folder:",stats.atime.toISOString().split('T'));
+    console.log("Change time of folder:", stats.ctime.toISOString().split('T'));
     console.log("Is file:", stats.isFile());
     console.log("Is directory:", stats.isDirectory());
-});
+    }
+)
