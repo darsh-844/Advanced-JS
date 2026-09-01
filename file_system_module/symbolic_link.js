932d3fbb1f6eb3e7d51ef3a9a9155f8b8701aaee
darsh-844 <darshjaiswal2007@gmail.com>
2026-09-01 12:51:01 +0530

    CRUD

--- file_system_module/symbolic_link.js
+++ file_system_module/symbolic_link.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.symlink("notes.txt", "link.txt", (err) => {
+fs.symlink("notes.txt", "link.txt", "dir", (err) => {
     if (err) {
         console.log(err);
         return;
