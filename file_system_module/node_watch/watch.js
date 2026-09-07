5f5756635a501242b87f061b2df8ead1e117927d
TheOG <fix@Darshs-MacBook-Air.local>
2026-09-07 09:25:55 +0530

    Watch

---
+++ file_system_module/node_watch/watch.js
@@ -0,0 +1,16 @@
+const fs=require('fs');
+
+// fs.watch('index.txt',(eventType,filename)=>{
+//     console.log(`Event Type: ${eventType}`);
+//     console.log(`Filename: ${filename}`);
+// });
+
+const watcher=fs.watch('index.txt',(eventType,filename)=>{
+    console.log(`Event Type: ${eventType}`);
+    console.log(`Filename: ${filename}`);
+});
+
+setTimeout(()=>{
+    watcher.close();
+    console.log('Stopped watching the file.');
+},10000);  
