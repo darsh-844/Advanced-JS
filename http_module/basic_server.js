9c4f3b385dfa6191e682f766ebe380e11ac5c346
darsh-844 <darshjaiswal2007@gmail.com>
2026-09-14 21:56:19 +0530

    SERVER

--- http_module/basic_server.js
+++ http_module/basic_server.js
@@ -1,48 +1,75 @@
-import http from 'http';
-
-// const http = require('http');
-import fs from 'fs';
-
-const server = http.createServer((req, res) => {
-    console.log("Yoooooooö");
-    
-    const order={
-        orderId: 9942,
-        des: "Delhi",
-        source: "Mumbai",
-        username: "Darsh",
-    }
-
-    // res.statusCode = 201;
-    // res.setHeader("Content-Type" , "application/json");
-
-    res.writeHead(200, {
-        'Content-Type': 'application/json', 
-        'Custom-Header': 'Darsh',
-        'accept-charset': 'utf-8',
-    });
-
-    fs.readFile('index.html','utf8', (err, data) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(data);
-            }
-        })
-
-    res.end(JSON.stringify(order));
-});
-
 // Status Code: 200 - ok
 // Status Code: 201 - create
 // Status Code: 400 - bad request
 // Status Code: 401 - unauthorized
 // Status Code: 403 - forbidden
 // Status Code: 404 - not found
+// Status Code: 408 - request timeout
+// Status Code: 411 - length required
 // Status Code: 500 - internal server error
 
-server.listen(5500,"127.0.0.1",()=>{
+// import http from 'http';
+// import fs from 'fs';
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+
+    console.log("Yoooooooö");
+
+    // const order = {
+    //     orderId: 9942,
+    //     des: "Delhi",
+    //     source: "Mumbai",
+    //     username: "Darsh",
+    // }
+
+    // res.statusCode = 201;
+    // res.setHeader("Content-Type", "application/json");
+    // res.writeHead(200, {
+    //     'Content-Type': 'application/json',
+    //     'Custom-Header': 'Darsh',
+    //     'accept-charset': 'utf-8',
+    // });
+
+    // res.end(JSON.stringify(order));
+
+    if (req.url === '/' || req.url === '/index.html') {
+
+        const filePath = path.join(__dirname, 'index.html');
+
+        fs.readFile(filePath, 'utf8', (err, data) => {
+
+            if (err) {
+                console.log(err);
+
+                res.writeHead(500, {
+                    'Content-Type': 'text/plain'
+                });
+
+                res.end('Internal Server Error');
+                return;
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+
+            res.end(data);
+        });
+
+    } else {
+
+        res.writeHead(404, {
+            'Content-Type': 'text/plain'
+        });
+
+        res.end('404 - Page Not Found');
+    }
+});
+
+server.listen(5500, "127.0.0.1", () => {
     console.log("Server is running on port 5500");
 })
