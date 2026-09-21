d8c94fe88a67b724e9b75e6212c6f15112c1fd99
darsh-844 <darshjaiswal2007@gmail.com>
2026-09-21 10:06:28 +0530

    Server

--- http_module/http_methods.js
+++ http_module/http_methods.js
@@ -6,12 +6,12 @@ const home = fs.readFileSync('./index.html' , 'utf-8');
 
 const server = http.createServer((req,res) => {
     console.log('Success!!!!');
-    if (req.url === '/config'){
+    if (req.url === '/' || req.url === '/home'){
+        res.end(home);
+    }
+    else if (req.url === '/config'){
         res.writeHead( 200 , {'Content-Type' : 'application/json'});
         res.end(JSON.stringify(dom));
-    }
-    else if (req.url === '/' || req.url === '/home'){
-        res.end(home);
     }
     else{
     res.end('Yoooooooo');
