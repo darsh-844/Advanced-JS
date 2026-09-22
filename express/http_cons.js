030ec103022715a6323bf8f5d277e12ac73fc6f4
darsh-844 <darshjaiswal2007@gmail.com>
2026-09-22 11:51:10 +0530

    Server

--- express/http_cons.js
+++ express/http_cons.js
@@ -2,11 +2,18 @@ import http from 'http';
 
 const server = http.createServer((req,res) => {
     console.log('Success!!!!!!!!');
-    if(req.method === 'GET' && req.url === '/'){
+    if (req.method === 'GET' && req.url === '/'){
         res.end('GET Request');
     }
     else if (req.method === 'POST' && req.url === '/'){
         res.end('POST Request');
+    }
+    else if (req.method === 'PATCH' && req.url === '/users'){
+        res.end('Patch Request');
+    }
+    else{
+        res.statusCode = 404;
+        res.end('Err page not found');
     }
 });
 
